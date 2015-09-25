7467af90975b9caa36c5a53aff48e76ed64f02b9
Kirill Simonov <xi@resolvent.net>
2015-09-25 15:30:11 -0500

    Fixing slides for readthedocs.org.

--- doc/conf.py
+++ doc/conf.py
@@ -35,20 +35,33 @@ slide_theme_options = {
 autoslides = False
 slides_in_html = True
 
+if 'slides_in_html' in tags:
+    slides_in_html = False
+
 def setup(app):
     app.add_config_value('slides_in_html', False, 'html')
     app.connect('html-collect-pages', _build_slides)
 
+from sphinx.builders.html import StandaloneHTMLBuilder
+original_script_files = StandaloneHTMLBuilder.script_files[:]
+original_css_files = StandaloneHTMLBuilder.css_files[:]
+
 def _build_slides(app):
     if app.builder.format != 'html' or not app.config.slides_in_html:
         return []
+    current_script_files = StandaloneHTMLBuilder.script_files
+    current_css_files = StandaloneHTMLBuilder.css_files
+    StandaloneHTMLBuilder.script_files = original_script_files
+    StandaloneHTMLBuilder.css_files = original_css_files
     from sphinx.application import Sphinx
     slides_dir = os.path.join(app.outdir, '_slides')
     slides_app = Sphinx(
-            app.srcdir, app.confdir, slides_dir, app.doctreedir,
-            'slides', { 'slides_in_html': False, 'html_theme': 'slides2' },
+            app.srcdir, app.confdir, slides_dir, app.doctreedir, 'slides',
             status=None, warning=app._warning,
-            warningiserror=app.warningiserror)
+            warningiserror=app.warningiserror,
+            tags=['slides_in_html'])
     slides_app.build()
+    StandaloneHTMLBuilder.script_files = current_script_files
+    StandaloneHTMLBuilder.css_files = current_css_files
     return []
 
